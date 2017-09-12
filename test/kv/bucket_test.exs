e6cd7f93db45c154b8707f38fabdebd675f722a3
Anthony McDonald <a@awews.com>
2017-09-12 17:59:56 -0600

    complete the agent section of the tutorial

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,41 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do # this is the callback that wasn't in the previous version of the test
+    {:ok, bucket} = start_supervised(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    # `bucket` is now the bucket from the setup block
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes `key` from `bucket`, if `key` exists", %{bucket: bucket} do
+
+    assert KV.Bucket.get(bucket, "leche") == nil
+
+    KV.Bucket.put(bucket, "leche", 4)
+    assert KV.Bucket.get(bucket, "leche") == 4
+
+    KV.Bucket.delete(bucket, "leche")
+
+    assert KV.Bucket.get(bucket, "leche") == nil
+  end
+end
+
+# this version doesn't use callbacks
+# defmodule KV.BucketTest do
+#   use ExUnit.Case, async: true
+#
+#   test "stores values by key" do
+#     {:ok, bucket} = start_supervised KV.Bucket
+#     assert KV.Bucket.get(bucket, "milk") == nil
+#
+#     KV.Bucket.put(bucket, "milk", 3)
+#     assert KV.Bucket.get(bucket, "milk") == 3
+#   end
+# end
