eae0635794beaad0d94b15f8759838a12c3c1898
Anthony McDonald <a@awews.com>
2017-09-13 22:35:23 -0600

    write parse function to pass test written using doctest

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -5,7 +5,13 @@ defmodule KVServer.Command do
   iex> KVServer.Command.parse "CREATE shopping\\r\\n"
   {:ok, {:create, "shopping"}}
   """
-  def parse(_line) do
-    :not_implemented
+  # def parse(_line) do
+  #   :not_implemented
+  # end
+
+  def parse(line) do
+    case String.split(line) do
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+    end
   end
 end
