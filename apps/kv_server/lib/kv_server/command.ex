8710617ac1c185d1a030da1c419a8c3908e3a97d
Anthony McDonald <a@awews.com>
2017-09-14 15:31:12 -0600

    complete doctests section

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -1,41 +1,4 @@
 defmodule KVServer.Command do
-  @doc """
-  Runs the given command.
-  """
-  def run(command)
-
-  def run({:create, bucket}, pid) do
-    KV.Registry.create(pid, bucket)
-    {:ok, "OK\r\n"}
-  end
-
-  def run({:get, bucket, key}, pid) do
-    lookup bucket, fn pid ->
-      value = KV.Bucket.get(pid, key)
-      {:ok, "#{value}\r\nOK\r\n"}
-    end
-  end
-
-  def run({:put, bucket, key, value}, pid) do
-    lookup bucket, fn pid ->
-      KV.Bucket.put(pid, key, value)
-      {:ok, "OK\r\n"}
-    end
-  end
-
-  def run({:delete, bucket, key}, pid) do
-    lookup bucket, fn pid ->
-      KV.Bucket.delete(pid, key)
-      {:ok, "OK\r\n"}
-    end
-  end
-
-  defp lookup(bucket, callback) do
-    case KV.Registry.lookup(KV.Registry, bucket) do
-      {:ok, pid} -> callback.(pid)
-      :error -> {:error, :not_found}
-    end
-  end
   @doc ~s"""
   Parses the given `line` into a command.
 
@@ -58,7 +21,7 @@ defmodule KVServer.Command do
 
   Unkown commands or commands with the wrong number of arguments return an error:
 
-  iex> KVServer.Command.parse "UNKNOWN shopping exggs\\r\\n"
+  iex> KVServer.Command.parse "UNKNOWN shopping eggs\\r\\n"
   {:error, :unknown_command}
 
   iex> KVServer.Command.parse "GET shopping\\r\\n"
@@ -106,6 +69,44 @@ defmodule KVServer.Command do
       ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
       ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
       _ -> {:error, :unknown_command}
+    end
+  end
+
+  @doc """
+  Runs the given command.
+  """
+  def run(command)
+
+  def run({:create, bucket}, pid) do
+    KV.Registry.create(pid, bucket)
+    {:ok, "OK\r\n"}
+  end
+
+  def run({:get, bucket, key}, pid) do
+    lookup bucket, fn pid ->
+      value = KV.Bucket.get(pid, key)
+      {:ok, "#{value}\r\nOK\r\n"}
+    end
+  end
+
+  def run({:put, bucket, key, value}, pid) do
+    lookup bucket, fn pid ->
+      KV.Bucket.put(pid, key, value)
+      {:ok, "OK\r\n"}
+    end
+  end
+
+  def run({:delete, bucket, key}, pid) do
+    lookup bucket, fn pid ->
+      KV.Bucket.delete(pid, key)
+      {:ok, "OK\r\n"}
+    end
+  end
+
+  defp lookup(bucket, callback) do
+    case KV.Registry.lookup(KV.Registry, bucket) do
+      {:ok, pid} -> callback.(pid)
+      :error -> {:error, :not_found}
     end
   end
 end
--- apps/kv_server/test/kv_server/command_test.exs
+++ apps/kv_server/test/kv_server/command_test.exs
@@ -1,4 +1,10 @@
 defmodule KVServer.CommandTest do
   use ExUnit.Case, async: true
   doctest KVServer.Command
+
+  def parse(line) do
+    case String.split(line) do
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+    end
+  end
 end
