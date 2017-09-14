e846970f57927924b77728087a39df54143ff482
Anthony McDonald <a@awews.com>
2017-09-13 22:29:06 -0600

    create test using doctest

---
+++ apps/kv_server/lib/kv_server/command.ex
@@ -0,0 +1,11 @@
+defmodule KVServer.Command do
+  @doc ~s"""
+  Parses the given `line` into a command.
+  ## Examples
+  iex> KVServer.Command.parse "CREATE shopping\\r\\n"
+  {:ok, {:create, "shopping"}}
+  """
+  def parse(_line) do
+    :not_implemented
+  end
+end
