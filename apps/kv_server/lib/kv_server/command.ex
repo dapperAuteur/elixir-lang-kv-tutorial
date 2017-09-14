bea2423e6d02c3a23c54637d12a05ec7da57e8d9
Anthony McDonald <a@awews.com>
2017-09-14 09:41:50 -0600

    rewrite KVServer.Command.run/1's to pass the server as an argument

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -4,26 +4,26 @@ defmodule KVServer.Command do
   """
   def run(command)
 
-  def run({:create, bucket}) do
-    KV.Registry.create(KV.Registry, bucket)
+  def run({:create, bucket}, pid) do
+    KV.Registry.create(pid, bucket)
     {:ok, "OK\r\n"}
   end
 
-  def run({:get, bucket, key}) do
+  def run({:get, bucket, key}, pid) do
     lookup bucket, fn pid ->
       value = KV.Bucket.get(pid, key)
       {:ok, "#{value}\r\nOK\r\n"}
     end
   end
 
-  def run({:put, bucket, key, value}) do
+  def run({:put, bucket, key, value}, pid) do
     lookup bucket, fn pid ->
       KV.Bucket.put(pid, key, value)
       {:ok, "OK\r\n"}
     end
   end
 
-  def run({:delete, bucket, key}) do
+  def run({:delete, bucket, key}, pid) do
     lookup bucket, fn pid ->
       KV.Bucket.delete(pid, key)
       {:ok, "OK\r\n"}
