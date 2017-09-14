f66b4e7730705bafd1c6856a0ff27a73157e9587
Anthony McDonald <a@awews.com>
2017-09-13 21:39:14 -0600

    Add IO.puts to functions to track order

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -28,6 +28,7 @@ defmodule KVServer do
     # 2. `packet: :line` -receives data line by line
     # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
     # 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
+    IO.puts("I'm here.")
 
     {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info "Accepting connections on port #{port}"
@@ -35,6 +36,7 @@ defmodule KVServer do
   end
 
   defp loop_acceptor(socket) do
+    IO.puts("'Now we can get started.' -loop_acceptor")
     {:ok, client} = :gen_tcp.accept(socket)
     {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
     :ok = :gen_tcp.controlling_process(client, pid)
@@ -50,11 +52,13 @@ defmodule KVServer do
   end
 
   defp read_line(socket) do
+    IO.puts("I'm reading.")
     {:ok, data} = :gen_tcp.recv(socket, 0)
     data
   end
 
   defp write_line(line, socket) do
+    IO.puts("I'm writing.")
     :gen_tcp.send(socket, line)
   end
 end
